c52fa5b94bb95e3024cb02454f74792e931c8105
Vishesh <vishesh.gubrani@gmail.com>
2025-02-11 13:38:27 +0530

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,36 @@
-import { defineConfig, sharpImageService } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import { readFileSync } from "node:fs";
-import mdx from '@astrojs/mdx';
-import compressor from "astro-compressor";
+import { defineConfig, sharpImageService } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import { readFileSync } from 'node:fs'
+import mdx from '@astrojs/mdx'
+import compressor from 'astro-compressor'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), compressor(), mdx()],
   image: {
-    service: sharpImageService()
+    service: sharpImageService(),
   },
-  site: "https://cojocarudavid.me",
+  site: 'https://visheshgubrani.com',
   vite: {
-    plugins: [rawFonts([".ttf", ".woff"])],
+    plugins: [rawFonts(['.ttf', '.woff'])],
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"]
-    }
-  }
-});
+      exclude: ['@resvg/resvg-js'],
+    },
+  },
+})
 
 // vite plugin to import fonts
 function rawFonts(ext) {
   return {
-    name: "vite-plugin-raw-fonts",
+    name: 'vite-plugin-raw-fonts',
     transform(_, id) {
-      if (ext.some(e => id.endsWith(e))) {
-        const buffer = readFileSync(id);
+      if (ext.some((e) => id.endsWith(e))) {
+        const buffer = readFileSync(id)
         return {
           code: `export default ${JSON.stringify(buffer)}`,
-          map: null
-        };
+          map: null,
+        }
       }
-    }
-  };
+    },
+  }
 }
--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,13 +1,25 @@
-export const tag = "@cojocaru-david";
-export const author = "David Cojocaru";
+export const tag = '@visheshgubrani'
+export const author = 'Vishesh Gubrani'
 
 export const defaultMeta = {
-  title: "David Cojocaru - Home",
+  title: 'Vishesh Gubrani - Home',
   description:
-    "Discover the portfolio and blog of David Cojocaru, a 16-year-old software developer and cybersecurity enthusiast from Romania. Explore his expertise in web development, programming languages, and more.",
-  keywords: ["david cojocaru", "junior software developer", "junior", "cybersecurity enthusiast", "web development", "programming", "portfolio", "blog", "romania"],
+    'Discover the portfolio and blog of Vishesh Gubrani, a 24-year-old software developer and devops enthusiast from India. Explore his expertise in web development, devops, programming languages, and more.',
+  keywords: [
+    'vishesh gubrani',
+    'junior software developer',
+    'junior',
+    'devops enthusiast',
+    'web development',
+    'programming',
+    'portfolio',
+    'blog',
+    'india',
+    'devops',
+    'cloud engineer',
+  ],
   ogImage: {
-    src: "/banner.png",
-    alt: "David Cojocaru - Home",
+    src: '/banner.png',
+    alt: 'Vishesh Gubrani - Home',
   },
-};
+}
